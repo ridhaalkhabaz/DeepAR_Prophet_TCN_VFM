6d096f02ce4eda2786be0acd27494f448da02897
ridhaalkhabaz <46946471+ridhaalkhabaz@users.noreply.github.com>
2021-08-11 07:58:58 -0500

    Add files via upload

--- src/configuration.py
+++ src/configuration.py
@@ -8,14 +8,38 @@ import pandas as pd
 import tensorflow as tf 
 from preprocessing import multivariate_data, univariate_data
 
-# to create a data set for our training and 
-def configure(data, params = [0, 4557, 5535, 7933], past_history=300, forecast_horizon=5, batch_size=64,  multi = False, column_index=7):
+# to create a tensor dataset for our training and validation. 
+def configure(data, params = [0, 4557,4557, 5535,  5535, 7933], past_history=300, forecast_horizon=5, batch_size=64,  multi = False, column_index=7):
+    """
+    This is a function to construct and make tenor data set for our training and validation. We usually use this function when we are 
+    certain about the cut offs in our original data set. The parameters are: 
+    - data : ARRAY-LIKE: the original data set.
+    - params: LIST: this list is to input the indexes for our training, validation and testing dataset. 
+        * params[0]: Integer: the starting index of the training data set. 
+        * params[1]: Integer: the ending index of the training data set. 
+        * params[2]: Integer: the starting index of the validation data set. 
+        * params[3]: Integer: the ending index of the validation data set. 
+        * params[4]: Integer: the starting index of the testing data set for the input of the network
+                        the starting index of the output is 300+parama[4]. 
+        * params[5]: Integer: the ending index of the testing data set.
+    - past_history: Integer: the number of observations you want to use for your input. 
+    - forecast_horizon: Integer: the number of time steps you want to forecast. 
+    - batch_size: Integer: batch size. 
+    - multi: BOOLEAN: whether you are using a single or multiple predictors. 
+    - column_index: Integer: the index for the target. 
+    IT returns:
+    - train_data_mult/train_data_uni: TENSOR DATASET: the training data set for our model. 
+    - val_data_mult/val_data_uni: TENSOR DATASET: the validation data set for our model. 
+    - testX_mult/testX_uni: ARRAY-LIKE: the input data for our testing data set. 
+    - testY_mult/testY_uni: ARRAY-LIKE: the targeted output.  
+    """
     if multi:
+        # this is to extract the desired input and output data from our data set
         x_mult, y_mult= multivariate_data(np.array(data), params[0], params[1], \
             past_history, forecast_horizon, column_index=column_index)
-        valX_mult , valY_mult = multivariate_data(np.array(data), params[1], \
-            params[2], past_history, forecast_horizon, column_index=column_index)
-        testX_mult, testY_mult = multivariate_data(np.array(data), params[2], params[3],\
+        valX_mult , valY_mult = multivariate_data(np.array(data), params[2], \
+            params[3], past_history, forecast_horizon, column_index=column_index)
+        testX_mult, testY_mult = multivariate_data(np.array(data), params[4], params[5],\
              past_history, forecast_horizon,  column_index=column_index)
         # to avoid combatibility issues
         x_mult = x_mult.astype(np.float32)
@@ -30,8 +54,8 @@ def configure(data, params = [0, 4557, 5535, 7933], past_history=300, forecast_h
         return train_data_mult, val_data_mult, testX_mult, testY_mult
     # for univariate 
     x_uni, y_uni = univariate_data(np.array(data), params[0], params[1], past_history, forecast_horizon)
-    valX_uni , valY_uni = univariate_data(np.array(data), params[1], params[2], past_history, forecast_horizon)
-    testX_uni, testY_uni = univariate_data(np.array(data),params[2], params[3], past_history, forecast_horizon)
+    valX_uni , valY_uni = univariate_data(np.array(data), params[2], params[3], past_history, forecast_horizon)
+    testX_uni, testY_uni = univariate_data(np.array(data),params[4], params[5], past_history, forecast_horizon)
     # to avoid combatibality issues
     x_uni = x_uni.astype(np.float32)
     y_uni = y_uni.astype(np.float32)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,13 +9,17 @@ import pandas as pd
 from pandas import read_csv
 from sklearn.preprocessing import MinMaxScaler
 import datetime as dttm
+from matplotlib import pyplot, rcParams, dates
+import matplotlib.pyplot as plt
 
 
 # Convert the sequence of seconds into datetime array
 def parser(x):
     """
-    This is a parser for seconds. parameters are: 
-    x : ARRAY-LIKE: the seconds columns. 
+    This is a parser for seconds. The parameters are: 
+    - x : ARRAY-LIKE: the seconds columns. 
+    IT returns:
+    - z: ARRAY-LIKE: the array of seconds 
     """
     now_seconds = 0
     y = x.astype(np.float) + now_seconds
@@ -23,16 +27,22 @@ def parser(x):
     return z
 
 # Read the data from fname and eventually plot them
-def read_data_slugging(fname,params=[10, 0, 5e-3, 10],is_not_clean=False):
+def read_data_slugging(fname,params=[10, 0, 5e-3, 10],is_not_clean=False, plot_data=False):
     """
-    we are normalizing and add some removed values, gaussian noise, and spikes. parameters are:
+    we are normalizing and add some removed values, gaussian noise, and spikes. The parameters are:
     - fname: STRING: the name of the file or a path to it
     - params: the parameters used for spikes, gaussian noise, and removed values, it goes like the following:
-        - params[0]: is the number of spikes desired to be added to our data set.
-        - params[1]: is the mean for the gaussian noise 
-        - params[2]: is the standard deviation for the gaussian noise 
-        - params[3]: is the number of removed values desired to be added to our data set.
+        - params[0]: Integer: is the number of spikes desired to be added to our data set.
+        - params[1]: Integer: is the mean for the gaussian noise 
+        - params[2]: Integer: is the standard deviation for the gaussian noise 
+        - params[3]: Integer: is the number of removed values desired to be added to our data set.
     - is_not_clean: BOOLEAN: whether you want to clean the data set or not. 
+    - plot_data: BOOLEAN: whether you want to plot the original data
+    IT returns:
+    - df: DATAFRAME: the organized pandas data frame of the original data . 
+    - data: ARRAY-LIKE: the original data. 
+    - headers: LIST: the list of column names of the original data set. 
+    - scaler: SCALER: the scaler used for normalization
     """
     # Read the time series
     datats = read_csv(fname, header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
@@ -50,11 +60,43 @@ def read_data_slugging(fname,params=[10, 0, 5e-3, 10],is_not_clean=False):
     # Replace timestamps with seconds
     time_sec = array([data[i, 0].timestamp() for i in range(len(data))])
     data = np.c_[time_sec, data[:, 1:]]
-
+    
+    # this is to test robustness in a vague sense. 
     if is_not_clean:
         # Adding spikes:
         data = spikes_addition(data, num_points=int(params[0]), is_not_well=True)
-
+    # to plot the data 
+    if (plot_data):
+        color = pyplot.rcParams['axes.prop_cycle'].by_key()['color']
+        fig0, ax = pyplot.subplots()
+        # pressure and temperature readings 
+        ax.plot(data[:, 0], data[:, 1:8])
+        ax.set_xlabel(headers[0])
+        ax.set_ylabel('Pressure (bar)')
+        headersplot = [w[-8:-2] for w in headers[1:8]]
+        px = ['$p(x_{%d}' % i for i in range(1, 8)]
+        tail = [')$'] * 7
+        headersplot = [px + headersplot + tail for px, headersplot, tail in zip(px, headersplot, tail)]
+        ax.legend(headersplot)
+        ax.set_title('Distributed pressure readings')
+        fig0.savefig('pressure-readings-slugging.jpeg')
+        # mass rate measurements 
+        fig, ax1 = pyplot.subplots()
+        
+        ax2 = ax1.twinx()
+        ln1 = ax1.plot(data[:, 0], data[:, 8], color=color[1], label='Gass mass rate (kg/sec)')
+        ln2 = ax2.plot(data[:, 0], data[:, 9], color=color[2], label='Liquid mass rate (kg/sec)')
+        pyplot.subplots_adjust(right=0.85)
+        ax1.set_xlabel('Time (sec)')
+        ax1.set_ylabel('Gas Mass rate (kg/sec)', color=color[1])
+        ax1.tick_params(axis='y', colors=color[1])
+        ax2.set_ylabel('Liquid Mass rate (kg/sec)', color=color[2])
+        ax2.tick_params(axis='y', colors=color[2])
+        lns = ln1 + ln2
+        labs = [l.get_label() for l in lns]
+        ax1.legend(lns, labs, loc='upper left')
+        ax1.set_title('Mass Flowrates (kg/sec)')
+        fig.savefig('mass-flowrates-slugging.jpeg')
 
     # Normalize the data
     scaler = MinMaxScaler(feature_range=(0, 1))
@@ -69,21 +111,28 @@ def read_data_slugging(fname,params=[10, 0, 5e-3, 10],is_not_clean=False):
     scaler.min_[pind] = scaler.feature_range[0] - pmin * scaler.scale_[pind]
 
     data = scaler.transform(data)
+    # this is to test robustness. 
     if is_not_clean:
         # Adding gaussian noise 
         data = gaussian_noise_addition(data, mu=params[1], sigma=params[2])
          # Removing some points:
         data, _ = random_missing_data(data, num_points=int(params[3]))
-    return data, scaler
+    df = pd.DataFrame.from_dict(dict(zip(['ds', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7','gas(kg/sec)', 'liquid(kg/sec)' ], data.T)))
+    return df, data, headers, scaler
+
 
 # This function is to add spikes to the data before normalization
 def spikes_addition(data, num_points=0, is_not_well=False):
     """
     Here, we add spikes to the orginal data per column. the spikes range is within
-    the maximum and minimum of each column. The parameters are:
+    the maximum and minimum of each column. Here, we hard code the columns for convenience purposes.  the  The parameters are:
+    - data: ARRAY-LIKE: the original data set. 
     - num_points: LIST: the number of observations to be missed.
     - is_well: BOOLEAN: is the data a slugging data set or a well data set
+    IT returns:
+    - data: ARRAY-LIKE: the modified data. 
     """
+    ## TODO try not hard coding by using .shape
     for i in random.sample(range(0, len(data)), num_points):
         # we begin at the second column because we assume that the first column is time 
         data[i][1] = np.random.uniform(data[:,1].min(),data[:,1].max())
@@ -102,8 +151,11 @@ def spikes_addition(data, num_points=0, is_not_well=False):
 def gaussian_noise_addition(data, mu=0, sigma=0.005):
     """
     we are simply adding gaussain noise to our data set. parameters are:
+    - data: ARRAY-LIKE: the original data set. 
     - mu: FLOAT: the mean. 
     - sigma: FLOAT: the standard deviation. 
+    IT returns: 
+    - data: ARRAY-LIKE: the modified data. 
     """
     # we begin at the second column because we assume that the first column is time 
     noise = np.random.normal(mu, sigma, data[:, 1:].shape)
@@ -114,7 +166,10 @@ def gaussian_noise_addition(data, mu=0, sigma=0.005):
 def random_missing_data(data, num_points=0):
     """
     we are removing some observations. parameters are:
-    -num-points: INTEGER: the number of observations you want to remove from the data set. 
+    - data: ARRAY-LIKE: the original data set. 
+    - num_points: INTEGER: the number of observations you want to remove from the data set. 
+    IT returns: 
+    - data: ARRAY-LIKE: the modified data. 
     """
     from random import randint
     index = [randint(0, len(data)) for i in range(num_points)]
@@ -130,7 +185,10 @@ def univariate_data(tseries, start_index, end_index, history_size, forecast_hori
     - start_index:INTEGER: the starting point is at
     - end_index:INTEGER: the ending point is at 
     - hisotry_size_:INTEGER: the number of past observations to use in our training for a single prediction. 
-    - forecast_horizon:INTEGER: the number of future points you want to predict. 
+    - forecast_horizon: INTEGER: the number of future points you want to predict. 
+    IT returns: 
+    - data: ARRAY-LIKE: the desired input for univariate model, i.e. past readings or pressure or temperature. 
+    - labels: ARRAY-LIKE: the target for univariate model, i.e. gas/water/oil flow rate. 
     """
 
     data = []
@@ -152,20 +210,23 @@ def multivariate_data(tseries, start_index, end_index, history_size, forecast_ho
     - tseries: NP.ARRAY: the data set
     - start_index:INTEGER: the starting point is at
     - end_index:INTEGER: the ending point is at 
-    - hisotry_size_:INTEGER: the number of past observations to use in our training for a single prediction. 
+    - hisotry_size :INTEGER: the number of past observations to use in our training for a single prediction. 
     - forecast_horizon:INTEGER: the number of future points you want to predict. 
     - column_index: INTEGER: the index of the targeted column 
+    IT returns:
+    - data: ARRAY-LIKE: the desired input for multivariate model, i.e. past readings or pressure or temperature or time. 
+    - labels: ARRAY-LIKE: the target for multivariate model, i.e. gas/water/oil flow rate. 
     """
     data = []
     labels = []
     for i in range(start_index, end_index -history_size +1, forecast_horizon):
         end = i+history_size
-        # if len(tseries[end:end+forecast_horizon, wanted:])
-        data.append(tseries[i:end, :])
+        # here we assume that the targeted column has the input before it. Hence that is why we have it like the following. 
+        data.append(tseries[i:end, :column_index])
         labels.append(tseries[end:end+forecast_horizon, column_index:])
     return np.array(data), np.array(labels)
 
-def read_data_well(fname, params=[0, 0, 5e-3, 100], is_not_clean=False):
+def read_data_well(fname, params=[0, 0, 5e-3, 100], is_not_clean=False, plot_data=False):
     """
     we are normalizing and add some removed values, gaussian noise, and spikes. parameters are:
     - fname: STRING: the name of the file or a path to it
@@ -175,9 +236,14 @@ def read_data_well(fname, params=[0, 0, 5e-3, 100], is_not_clean=False):
         - params[2]: is the standard deviation for the gaussian noise 
         - params[3]: is the number of removed values desired to be added to our data set.
     - is_not_clean: BOOLEAN: whether you want to clean the data set or not. 
+    - plot_data: BOOLEAN: whether you want to plot the original data set as two figures, one for the input and one for the output. 
+    IT returns: 
+    - data: ARRAY-LIKE: the original data set. 
+    - headers: LIST: the names of the columns of the original data set. 
+    - scaler: SCALER: the scaler used to normlaize the data. 
     """
     # Read the time series
-    datats = read_csv(fname, header=0, dayfirst=True, parse_dates=[0], index_col=0, squeeze=True)  # , date_parser=parser
+    datats = read_csv(fname, header=0, dayfirst=True, parse_dates=[0], index_col=0, squeeze=True) 
 
     headers = list(datats.columns.values)
     headers.insert(0, datats.index.name)
@@ -206,7 +272,66 @@ def read_data_well(fname, params=[0, 0, 5e-3, 100], is_not_clean=False):
         n0 = n1
         if n+1 < Nfp:
             n1 = ind[n+1] + 1   
- 
+    # Plot the graphs
+    if (plot_data):
+
+        color = pyplot.rcParams['axes.prop_cycle'].by_key()['color']
+        dfmt = dates.DateFormatter('%b %d') # Month day
+
+        # Pressure and temperature
+        fig1, ax1 = pyplot.subplots()
+        ax2 = ax1.twinx()
+        for n in range(Nfp):
+            if n == 0:
+                hl1 = ax1.plot(fp[n][:, 0], fp[n][:, 1], color=color[1], label='Pressure')
+                hl2 = ax2.plot(fp[n][:, 0], fp[n][:, 2], color=color[2], label='Temperature')
+            else:
+                ax1.plot(fp[n][:, 0], fp[n][:, 1], color=color[1])
+                ax2.plot(fp[n][:, 0], fp[n][:, 2], color=color[2])
+
+        ax1.xaxis.set_major_formatter(dfmt)
+        fig1.autofmt_xdate()
+        ax1.set_ylabel(headers[1], color=color[1])
+        ax1.tick_params(axis='y', colors=color[1])
+        headers[2] = headers[2].replace('degC', '°C')
+        ax2.set_ylabel(headers[2], color=color[2])
+        ax2.tick_params(axis='y', colors=color[2])
+
+        hl = hl1 + hl2
+        labs = [h.get_label() for h in hl]
+        ax1.legend(hl, labs, loc=2)
+        ax1.set_title('Pressure and temperature data')
+        fig1.savefig('pressure-temperature-readings-welltest.jpeg')
+
+        # Flow rates
+        fig2, ax1 = pyplot.subplots()
+        ax2 = ax1.twinx()
+        for n in range(Nfp):
+            if n == 0:
+                hl1 = ax1.plot(fp[n][:, 0], fp[n][:, 3], color=color[3], label='Oil rate')
+                hl2 = ax1.plot(fp[n][:, 0], fp[n][:, 4], color=color[4], label='Water rate')
+                hl3 = ax2.plot(fp[n][:, 0], fp[n][:, 5], color=color[5], label='Gas rate')
+            else:
+                ax1.plot(fp[n][:, 0], fp[n][:, 3], color=color[3])
+                ax1.plot(fp[n][:, 0], fp[n][:, 4], color=color[4])
+                ax2.plot(fp[n][:, 0], fp[n][:, 5], color=color[5])
+
+        ax1.xaxis.set_major_formatter(dfmt)
+        fig2.autofmt_xdate()
+        rheader = headers[3].split()[0] + ' & ' + headers[4]
+        ax1.set_ylabel(rheader, color=color[3])
+        ax1.tick_params(axis='y', colors=color[3])
+        ax2.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+        ax2.set_ylabel(headers[5], color=color[5])
+        ax2.tick_params(axis='y', colors=color[5])
+
+        hl = hl1 + hl2 + hl3
+        labs = [h.get_label() for h in hl]
+        ax1.legend(hl, labs, loc=1)
+        ax1.set_title('Flow rates data')
+        fig2.savefig('mass-flowrate-welltest.jpeg')
+
+
     # Get the normalization parameters for all data
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaler = scaler.fit(data[:,1:]) # Exclude Datetime from normalization
@@ -223,3 +348,70 @@ def read_data_well(fname, params=[0, 0, 5e-3, 100], is_not_clean=False):
             fp[n], _ = random_missing_data(fp[n], num_points=int(params[3]))
 
     return fp, headers, scaler
+# this function is used to get the difference between timestamps in seconds
+def get_sec(time_str):
+    """
+    this function is get amount of seconds since the start of the month. the parameters are:
+    - time_str: STRING: the timestamp
+    IT returns:
+    - Integer: the number of seconds since the start of the month. 
+    """
+    a, b = time_str.split(' ')
+    h, m, s = b.split(':')
+    k,c,n = a.split('-')
+    # here we assume that every operation period is within the same month 
+    return int(h) * 3600 + int(m) * 60 + int(s) + (int(n)-1)*60*60*24
+# this function is used to do some padding between operation periods. 
+def dataframe(fp, col_names, padding, timing):
+    """
+    this function is to transform the well's data and do some padding if needed. The parameters are:
+    - fp: LIST: the original data set, here is a list of array flow periods . 
+    - col_names: LIST: the names of the columns. 
+    - padding: BOOLEAN: whether you want to do some padding or not.
+    - timing: BOOLEAN: whether you want to add some increasing series to count for the time series. 
+    IT returns:
+    - df: DATAFRAME: the organized dataframe of the original data set. 
+    """
+    df = pd.DataFrame.from_dict(dict(zip(col_names, fp[0].T)))
+    for i in range(1, len(fp)):
+        tmp = pd.DataFrame.from_dict(dict(zip(col_names, fp[i].T)))
+        if padding:
+            time_a = str(df.iloc[-1].ds)
+            time_b = str(tmp.iloc[0].ds)
+            minutes = int((get_sec(time_b)-get_sec(time_a))/60) - 1
+            df_try = pd.DataFrame([df.iloc[-1]]*minutes)
+            df = pd.concat([df, df_try], axis=0)
+        df = pd.concat([df, tmp], axis=0)
+    if timing:
+        df['time'] = [i for i in range(len(df))]
+    return df
+# this function is to turn time into timestamp for our deepAR
+def time_to_timestamp(seconds):
+    """
+    this function is to turn seconds into time stamps. The parameters are:
+    - seconds: Integer: the number of seconds you have. 
+    IT returns: 
+    - timestamp: DATETIME TIMESTAMP: the time converted into a timestamp. 
+    """
+    addition = 1625893200
+    minutes = seconds * 60 
+    minutes += addition
+    timestamp = dttm.datetime.fromtimestamp(minutes)
+    return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+# this function is to plot the results from the DeepAR algorithm 
+def plot_prob_forecasts(ts_entry, forecast_entry):
+    """
+    This function is to plot the DeepAR results. The parameters are:
+    - ts_entry: LIST: the original testing data set. 
+    - forecast_entry: LIST: the forecasted observations. 
+    """
+    plot_length = 1000
+    prediction_intervals = (80.0, 95.0)
+    legend = ["observations", "median prediction"] + [f"{k}% prediction interval" for k in prediction_intervals][::-1]
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+    ts_entry[-plot_length:].plot(ax=ax)  # plot the time series
+    forecast_entry.plot(prediction_intervals=prediction_intervals, color='g')
+    plt.grid(which="both")
+    plt.legend(legend, loc="upper left")
+    plt.show()
